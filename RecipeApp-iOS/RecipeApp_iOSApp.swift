3d247773c092964c43b8b3b536bdff81d67f3e12
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-10-07 18:30:55 +0600

    Initial Commit

---
+++ RecipeApp-iOS/RecipeApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp_iOSApp.swift
+//  RecipeApp-iOS
+//
+//  Created by Sopnil Sohan on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
